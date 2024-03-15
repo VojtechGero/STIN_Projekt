02fdf76034f331902f286dd69fc21b98d241fcf7
VojtechGero <vojtech.gero@tul.cz>
2024-03-15 13:19:35 +0100

    testing now works

--- TestPlatba/UnitTest1.cs
+++ TestPlatba/UnitTest1.cs
@@ -1,4 +1,5 @@
 using API;
+using Microsoft.Extensions.Logging;
 using System.Text.Json;
 namespace TestPlatba
 {
@@ -8,14 +9,23 @@ namespace TestPlatba
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
             Platba p=new Platba();
             p.typ_platby = "CARD";
             p.castka = 1;
             p.mena = "EUR";
             string json=JsonSerializer.Serialize(p);
-            string test = "1 EUR";
-            var controller = API.Controllers.PlatbaController();
-            Assert.AreEqual(test, controller.Get(json));
+            string expected = "1 EUR";
+
+            var factory=new LoggerFactory();
+            var logger = factory.CreateLogger<API.Controllers.PlatbaController>();
+            var controller = new API.Controllers.PlatbaController(logger);
+
+            //Act
+            string actual= controller.Get(json);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
 
         }
     }
